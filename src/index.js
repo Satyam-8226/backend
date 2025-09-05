3b50d54a081b0b8ecadf21db6cfabcad0fd00919
Satyam Pandey <s.pandey274509@gmail.com>
2025-09-05 19:18:39 +0530

    Database connection files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require("dotenv").config({path: "./.env"});
+// will run file but it's not the best way to do it because it ruins consistencyof code
 
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB();
+
+// in package.json add -r dotenv/config to scripts start
+
+
+
+
+
+
+
+
+
+
+/* import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
 import express from "express"
 const app = express()
 
-/* ;(async () => {
+;(async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         app.on("error", (error) => {
